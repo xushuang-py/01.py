b360a2dcee65d2c3194a986002a4fa7c50437d4f
xushuang <3350709173@qq.com>
2026-08-18 15:56:25 +0800

    新增python练习脚本

--- 03.py
+++ 03.py
@@ -10,6 +10,37 @@
 #     turtle.goto(0,-100)
 #     turtle.pendown()
 #     turtle.circle(100,360)
-# turtle.done() 
+
+
+# turtle.done()
+# import decimal
+# a=decimal.Decimal("3.141592653589793")
+# b=decimal.Decimal("2.718281828459045")
+# decimal.getcontext().prec=21
+# print(a*b)
+
+
+# x=3.151592653589793
+# x**=3
+# print(x)
+# 太阳花绘制
+# from turtle import *
+# color('red','yellow')
+# begin_fill()
+# while True:
+#   forward(200)
+#   left(170)
+#   if abs(pos())<1:
+#     break
+# end_fill()
+# done()
+
+
+# print(10.0-1+2j)
+# x=-10
+# print(abs(x))
+# print(abs(-3+4j))
+
+
 
   
